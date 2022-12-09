cceb299f0eaa37aa591da6af5fc76ea699ac20c6
sakshi4321 <57297556+sakshi4321@users.noreply.github.com>
2022-12-09 17:27:25 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include "stm32f4xx_hal.h"
+
+void Configure_GPIO(void);
+void configure_ADC2_Channel_0(void);
+void Delay_ms(volatile int time_ms);
+double voltage;
+//ADC in single conversion mode( 1-2 samples per second only)
+uint32_t Adc_value;
+ADC_HandleTypeDef myADC2Handle;
+uint32_t counter;
+int main(void)
+{
+	Configure_GPIO(); // initialize PA0 pin 
+	configure_ADC2_Channel_0(); // configure ADC2
+	while(1)
+	{
+	 HAL_ADC_Start(&myADC2Handle); // start A/D conversion
+		if(HAL_ADC_PollForConversion(&myADC2Handle, 5) == HAL_OK) //check if conversion is completed
+		{
+		Adc_value  = HAL_ADC_GetValue(&myADC2Handle); // read digital value and save it inside uint32_t variable
+		voltage=(Adc_value * 8) / 10000;
+		
+		if (voltage > 0){
+			counter++;
+			if (counter >= 5){
+				//LED blinks
+				HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
+
+				// Wait for 500 ms
+				HAL_Delay(10000);
+
+				counter=0;
+			}
+		
+
+		}
+		else{
+			counter=0;
+		}
+		}
+		HAL_ADC_Stop(&myADC2Handle); // stop conversion 
+		Delay_ms(200);
+	}
+}
+
+/* Configure and initialize PA0 pin as analog input pin for A/D conversion */
+void Configure_GPIO(void)
+{
+	 __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock to GPIOA
+	GPIO_InitTypeDef ADCpin; //create an instance of GPIO_InitTypeDef C struct
+	ADCpin.Pin = GPIO_PIN_0; // Select pin PA0
+	ADCpin.Mode = GPIO_MODE_ANALOG; // Select Analog Mode
+	ADCpin.Pull = GPIO_NOPULL; // Disable internal pull-up or pull-down resistor
+	HAL_GPIO_Init(GPIOA, &ADCpin); // initialize PA0 as analog input pin
+}
+void configure_ADC2_Channel_0(void)
+{
+__HAL_RCC_ADC2_CLK_ENABLE(); // enable clock to ADC2 module
+	myADC2Handle.Instance = ADC2; // create an instance of ADC2
+	myADC2Handle.Init.Resolution = ADC_RESOLUTION_12B; // select 12-bit resolution 
+	myADC2Handle.Init.EOCSelection = ADC_EOC_SINGLE_CONV; //select  single conversion as a end of conversion event
+	myADC2Handle.Init.DataAlign = ADC_DATAALIGN_RIGHT; // set digital output data right justified
+	myADC2Handle.Init.ClockPrescaler =ADC_CLOCK_SYNC_PCLK_DIV8; 
+	HAL_ADC_Init(&myADC2Handle); // initialize AD2 with myADC2Handle configuration settings
+	
+  /*select ADC2 channel */
+	ADC_ChannelConfTypeDef Channel_AN0; // create an instance of ADC_ChannelConfTypeDef
+	Channel_AN0.Channel = ADC_CHANNEL_0; // select analog channel 0
+	Channel_AN0.Rank = 1; // set rank to 1
+	Channel_AN0.SamplingTime = ADC_SAMPLETIME_15CYCLES; // set sampling time to 15 clock cycles
+	HAL_ADC_ConfigChannel(&myADC2Handle, &Channel_AN0); // select channel_0 for ADC2 module. 
+}
+void Init_OnBoard_LEDs(void)
+{
+	 __HAL_RCC_GPIOD_CLK_ENABLE();
+	GPIO_InitTypeDef BoardLEDs;
+	BoardLEDs.Mode = GPIO_MODE_OUTPUT_PP;
+	BoardLEDs.Pin = GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+	HAL_GPIO_Init(GPIOD, &BoardLEDs);
+}
+
+void Delay_ms(volatile int time_ms)
+{
+	      int j;
+        for(j = 0; j < time_ms*4000; j++)
+            {}  /* excute NOP for 1ms */
+}
+
+
+
